090b415e2d9da1dfd0a713f26f2766190e41b49f
EliaNaCc <ync999@qq.com>
2019-11-18 23:16:23 +0800

    项目全面添加swagger2支持（解决bug）

--- src/main/java/org/cny/yurayura/config/SwaggerConfig.java
+++ src/main/java/org/cny/yurayura/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-@ConditionalOnExpression("${swagger.enable}")
+@ConditionalOnExpression("${yurayura.swagger.enable}")
 public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
