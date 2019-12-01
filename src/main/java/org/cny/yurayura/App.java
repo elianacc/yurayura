e8dc30651d4998ca9bfeb4c3ddf7cc73348322e2
EliaNaCc <ync999@qq.com>
2019-12-02 01:16:40 +0800

    fix: 启动类开启异步请求

--- src/main/java/org/cny/yurayura/App.java
+++ src/main/java/org/cny/yurayura/App.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync //开启异步调用
 @MapperScan(basePackages = {"org.cny.yurayura.dao"})
 public class App extends SpringBootServletInitializer {
 
