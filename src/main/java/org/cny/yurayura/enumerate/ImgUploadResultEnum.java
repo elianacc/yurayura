83ad4b537e837a6927d6212ed1ef3b8173da46e5
EliaNaCc <ync999@qq.com>
2020-01-27 14:16:17 +0800

    feat: 新增上传图片结果枚举

---
+++ src/main/java/org/cny/yurayura/enumerate/ImgUploadResultEnum.java
@@ -0,0 +1,33 @@
+package org.cny.yurayura.enumerate;
+
+import lombok.Getter;
+
+/**
+ * 图片上传结果 enum
+ *
+ * @author CNY
+ * @date 2020年01月26日 18:10
+ */
+@Getter
+public enum ImgUploadResultEnum {
+
+    /**
+     * 上传图片为空
+     */
+    NULL("0"),
+    /**
+     * 上传图片格式必须是.gif,jpeg,jpg,png中的一种
+     */
+    FORMATNOTALLOW("1"),
+    /**
+     * 上传图片不能超过100KB
+     */
+    SIZEBEYOND("2");
+
+    private String result;
+
+    ImgUploadResultEnum(String result) {
+        this.result = result;
+    }
+
+}
