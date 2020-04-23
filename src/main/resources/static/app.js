6cdedb4637ee2ae7850f4a43276f8c58b46ec0f5
cny <elianacc@126.com>
2020-04-23 17:54:24 +0800

    style: app.js修改，调整常用js，非常用在对应碎片下导入

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,13 +2,8 @@ document.write("<script src='/frame/jquery-1.12.4.min.js'></script>");
 document.write("<script src='/frame/vue.js'></script>");
 document.write("<script src='/frame/bootstrap.bundle.min.js'></script>");
 
-document.write("<script src='/plugins/base64.min.js'></script>");
 document.write("<script src='/plugins/jquery.form.min.js'></script>");
-document.write("<script src='/plugins/jquery.scrollUp.min.js'></script>");
-document.write("<script src='/plugins/jquery.unveil.js'></script>");
 document.write("<script src='/plugins/toastr.min.js'></script>");
-document.write("<script src='/plugins/BootSideMenu.js'></script>");
 document.write("<script src='/plugins/bootbox.min.js'></script>");
-document.write("<script src='/plugins/metisMenu.min.js'></script>");
 document.write("<script src='/plugins/bootstrap-datepicker.min.js'></script>");
 document.write("<script src='/plugins/bootstrap-datepicker.zh-CN.min.js'></script>");
