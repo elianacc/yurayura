b1439455b74c87a9884ccac1d64291bda04fdbcf
cny <elianacc@126.com>
2020-05-06 14:52:42 +0800

    fix: 去除项目的jquery.form插件，现在交互接口使用axios

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,7 +3,6 @@ document.write("<script src='/frame/vue.js'></script>");
 document.write("<script src='/frame/bootstrap.bundle.min.js'></script>");
 
 document.write("<script src='/plugins/axios.min.js'></script>");
-document.write("<script src='/plugins/jquery.form.min.js'></script>");
 document.write("<script src='/plugins/toastr.min.js'></script>");
 document.write("<script src='/plugins/bootbox.min.js'></script>");
 document.write("<script src='/plugins/bootstrap-datepicker.min.js'></script>");
