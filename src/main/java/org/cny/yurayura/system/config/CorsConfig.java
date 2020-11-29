34672633ac22272b7d131546574bfb2f7f9f3d19
EliaNaCc <ync999@qq.com>
2020-11-29 22:51:35 +0800

    fix: 增加处理跨域配置注释

--- src/main/java/org/cny/yurayura/system/config/CorsConfig.java
+++ src/main/java/org/cny/yurayura/system/config/CorsConfig.java
@@ -16,14 +16,12 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        /*
-         * 一小时内不需要再预检
-         */
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowCredentials(true)
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .maxAge(3600);
+
+        registry.addMapping("/**") // 所有的当前站点的请求地址，都支持跨域访问
+                .allowedOrigins("*") // 所有的外部域都可跨域访问
+                .allowCredentials(true) // 是否支持跨域用户凭证
+                .allowedHeaders(CorsConfiguration.ALL) // 当前站点支持的跨域请求头是什么
+                .allowedMethods(CorsConfiguration.ALL) // 当前站点支持的跨域请求类型是什么
+                .maxAge(3600); // 超时时长设置为1小时。 时间单位是秒
     }
 }
