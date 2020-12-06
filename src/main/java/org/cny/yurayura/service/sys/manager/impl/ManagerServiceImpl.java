8b5504666c326d5fb74a7ec82d3335a280b6de24
EliaNaCc <ync999@qq.com>
2020-12-07 02:03:58 +0800

    feat: 新增shiro配置类和自定义Realm，管理员登入及认证授权全部采用shiro

--- src/main/java/org/cny/yurayura/service/sys/manager/impl/ManagerServiceImpl.java
+++ src/main/java/org/cny/yurayura/service/sys/manager/impl/ManagerServiceImpl.java
@@ -1,17 +1,19 @@
 package org.cny.yurayura.service.sys.manager.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.cny.yurayura.dao.sys.manager.ManagerMapper;
 import org.cny.yurayura.dto.MangerLoginDto;
 import org.cny.yurayura.entity.sys.manager.Manager;
-import org.cny.yurayura.enumerate.ManagerStatusEnum;
 import org.cny.yurayura.service.sys.manager.IManagerService;
 import org.cny.yurayura.vo.ApiResult;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 import org.springframework.util.StringUtils;
@@ -31,47 +33,43 @@ import java.net.URLEncoder;
 @Service
 public class ManagerServiceImpl extends ServiceImpl<ManagerMapper, Manager> implements IManagerService {
 
-    @Autowired
-    private ManagerMapper managerMapper;
-
     @SneakyThrows
     @Override
-    public ApiResult login(MangerLoginDto dto, HttpSession session, HttpServletResponse response) {
+    public ApiResult login(MangerLoginDto dto, HttpSession httpSession, HttpServletResponse response) {
         // 获取服务器生成验证码
-        Object managerVerifyCode = session.getAttribute("managerVerifyCode");
+        Object managerVerifyCode = httpSession.getAttribute("managerVerifyCode");
         // 验证码session失效
-        if(StringUtils.isEmpty(managerVerifyCode)) {
+        if (StringUtils.isEmpty(managerVerifyCode)) {
             return ApiResult.warn("验证码过期，请刷新验证码");
         }
         if (managerVerifyCode.toString().equalsIgnoreCase(dto.getVerifyCode())) {
-            Manager manager = new Manager();
-            BeanUtils.copyProperties(dto, manager);
-            // MD5加密管理员密码
-            manager.setManagerPassword(DigestUtils.md5DigestAsHex(manager.getManagerPassword().getBytes()));
-            // 查询有没有匹配管理员
-            QueryWrapper<Manager> queryWrapper = new QueryWrapper<>();
-            Manager aManager = managerMapper.selectOne(queryWrapper
-                    .eq("manager_name", manager.getManagerName())
-                    .eq("manager_password", manager.getManagerPassword())
-                    .eq("manager_status", ManagerStatusEnum.ENABLE.getStatusId()));
-            if (!StringUtils.isEmpty(aManager)) {
-                // 登入成功存管理员session
-                session.setAttribute("managerSession", aManager);
-                // 以秒为单位，即在没有活动60分钟后，管理员session将失效
-                session.setMaxInactiveInterval(60 * 60);
+            // 封装用户登入数据(用户名+密码)为token
+            UsernamePasswordToken token = new UsernamePasswordToken(dto.getManagerName(), DigestUtils.md5DigestAsHex(dto.getManagerPassword().getBytes()));
+            // 获取当前用户
+            Subject subject = SecurityUtils.getSubject();
+            try {
+                // 当前用户登入
+                subject.login(token);
+                Manager currentManger = (Manager) subject.getPrincipal();
+                Session session = subject.getSession();
+                // 存入shiro的session
+                session.setAttribute("managerSession", currentManger);
                 // 将sessionId存入浏览器对应sessionId的cookie
-                Cookie cookie = new Cookie("JSESSIONID", URLEncoder.encode(session.getId(), "utf-8"));
+                Cookie cookie = new Cookie("JSESSIONID", URLEncoder.encode(String.valueOf(session.getId()), "utf-8"));
                 cookie.setPath("/");
                 // 设置sessionId cookie有效期为60分钟，关闭浏览器session也不会消失，记住登入
                 cookie.setMaxAge(60 * 60);
                 // js无法读取cookie，防止xss攻击
                 cookie.setHttpOnly(true);
                 response.addCookie(cookie);
-                log.info("管理员：{}，登入成功", aManager.getManagerName());
+                log.info("管理员：{}，登入成功", currentManger.getManagerName());
                 return ApiResult.success("管理员登入成功");
-            } else {
-                return ApiResult.warn("用户名或密码错误");
+            } catch (UnknownAccountException uae) {
+                return ApiResult.warn("用户不存在");
+            } catch (IncorrectCredentialsException ice) {
+                return ApiResult.warn("密码错误");
             }
+
         } else {
             return ApiResult.warn("验证码错误");
         }
