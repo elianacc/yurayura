efbea4fe934f0f2c80738406374fc293c55e77ac
elianacc <ync999@qq.com>
2024-12-18 10:53:10 +0800

    sync

--- yura-cloud-modules/yura-cloud-sys/src/main/java/pers/elianacc/yurayura/mqtt/MqttReceiveMsgHandler.java
+++ yura-cloud-modules/yura-cloud-sys/src/main/java/pers/elianacc/yurayura/mqtt/MqttReceiveMsgHandler.java
@@ -2,16 +2,18 @@ package pers.elianacc.yurayura.mqtt;
 
 import cn.hutool.json.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.mqtt.support.MqttHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.MessagingException;
 import org.springframework.stereotype.Component;
+import pers.elianacc.yurayura.component.MqttMessageSender;
 import pers.elianacc.yurayura.constants.MqttConstant;
 import pers.elianacc.yurayura.entity.SysNotice;
 import pers.elianacc.yurayura.service.ISysNoticeService;
-import pers.elianacc.yurayura.component.MqttMessageSender;
 
 /**
  * mqtt接收消息处理 component
@@ -20,13 +22,19 @@ import pers.elianacc.yurayura.component.MqttMessageSender;
  * @since 2024-12-16
  */
 @Component
-public class ReceiverMessageHandler implements MessageHandler {
+public class MqttReceiveMsgHandler implements MessageHandler {
 
     @Autowired
     private MqttMessageSender mqttMessageSender ;
 
     @Autowired
     private ISysNoticeService iSysNoticeService;
+
+    @Bean
+    @ServiceActivator(inputChannel = "messageInboundChannel")
+    public MessageHandler messageHandler() {
+        return this;
+    }
 
     @Override
     public void handleMessage(Message<?> message) throws MessagingException {
@@ -39,6 +47,6 @@ public class ReceiverMessageHandler implements MessageHandler {
             iSysNoticeService.save(notice);
             mqttMessageSender.sendMsg(MqttConstant.YURA_BUSINESS_VUE_NOTICE_PLUS, JSONUtil.toJsonStr(notice));
         }
-
     }
+
 }
